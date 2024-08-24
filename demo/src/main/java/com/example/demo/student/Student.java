ac84b1160d9a5373efb6cd145e7fc771dc2a601e
H5misi <hazm.sayed.m@gmail.com>
2024-08-24 05:05:21 +0300

    Fourth commit

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 // Map "student" class to database
 @Entity
@@ -23,6 +24,7 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+    @Transient
     private Integer age;
 
 //    Constructor without attributes
@@ -80,7 +82,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
